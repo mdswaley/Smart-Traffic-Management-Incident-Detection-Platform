8862259f729ade4891335bd76128deaca7cfe79b
mdswaley <mdswaley075@gmail.com>
2026-09-08 18:18:34 +0530

    adding mongodb dependency and configure in application.yaml

--- src/main/java/com/mdswaley/traffic/smart_traffic_management/Model/TrafficEvent.java
+++ src/main/java/com/mdswaley/traffic/smart_traffic_management/Model/TrafficEvent.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Setter
+@Document(collection = "traffic_events")
 public class TrafficEvent {
     private String intersectionId;
 
