21c6243dd120b3e6ad7435e9ecaee487196157c4
mdswaley <mdswaley075@gmail.com>
2026-09-08 12:35:22 +0530

    first commit

---
+++ src/main/java/com/mdswaley/traffic/smart_traffic_management/SmartTrafficManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mdswaley.traffic.smart_traffic_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTrafficManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTrafficManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdswaley/traffic/smart_traffic_management/SmartTrafficManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdswaley.traffic.smart_traffic_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTrafficManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
